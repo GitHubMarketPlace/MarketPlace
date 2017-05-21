33d60c7398e8b4e5839a44be5e0f97b8502c9e34
Francisco José Olvera Junior <olverajunior2014@gmail.com>
2017-05-21 16:52:16 -0300

    Checkpoint - Detalhe do produto view

--- app/src/main/java/marketplace/tcc/usjt/br/marketplace/activity/triggerDetalhes/DetalheProdutoActivity.java
+++ app/src/main/java/marketplace/tcc/usjt/br/marketplace/activity/triggerDetalhes/DetalheProdutoActivity.java
@@ -1,15 +1,13 @@
 package marketplace.tcc.usjt.br.marketplace.activity.triggerDetalhes;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,79 +17,48 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
-
 import marketplace.tcc.usjt.br.marketplace.R;
 import marketplace.tcc.usjt.br.marketplace.activity.triggerGlobal.CarrinhoActivity;
-import marketplace.tcc.usjt.br.marketplace.adapter.ProdutoCategoriaAdapter;
 import marketplace.tcc.usjt.br.marketplace.config.FirebaseConfig;
 import marketplace.tcc.usjt.br.marketplace.model.Produto;
 
-public class DetalheCategoriaActivity extends AppCompatActivity {
+public class DetalheProdutoActivity extends AppCompatActivity {
 
-    private TextView nomeCategoria;
-    private TextView labelQuatidade;
+    private TextView nomeProduto;
     private ProgressBar spinner;
-    private ListView productList;
-    private Activity context;
     private DatabaseReference reference;
     private Query queryRef;
-    private Bundle params;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalhe_categoria);
+        setContentView(R.layout.activity_detalhe_produto);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        context = this;
 
-        nomeCategoria = (TextView) findViewById(R.id.nome_categoria);
+        nomeProduto = (TextView) findViewById(R.id.nome_categoria);
         spinner = (ProgressBar)findViewById(R.id.progressBar4);
-
-        // Estrutura da lista de produtos
-        final ArrayList<Produto> list = new ArrayList<>();
-        final ProdutoCategoriaAdapter adapter = new ProdutoCategoriaAdapter(list, context);
-        productList = (ListView)findViewById(R.id.lista_produtos_categoria);
-        productList.setAdapter(adapter);
-        productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Cria uma interface bundle (tipo hashmap) para passar o nome doproduto para o intent
-                params = new Bundle();
-                params.putString("nomeProduto", list.get(position).getNome().toString());
-
-                // Passa o nome do produto para a view de detalhe
-                Intent detalheProduto = new Intent(context, DetalheProdutoActivity.class);
-                detalheProduto.putExtras(params);
-                startActivity(detalheProduto);
-            }
-        });
 
         // Verifica os dados vindos do intent de Categorias
         Intent intent = getIntent();
         if (intent != null){
             Bundle params = intent.getExtras();
             if(params != null){
-                String nome = params.getString("nomeCategoria");
-                // Seta o nome da categoria no TextView
-                nomeCategoria.setText(nome);
+                String nome = params.getString("nomeProduto");
 
+                Log.i("NOMEDOFDP", nome);
                 reference = FirebaseConfig.getFirebase().child("products");
-                queryRef =  reference.orderByChild("categoria").equalTo(nome);
+                queryRef =  reference.orderByChild("nome").equalTo(nome);
 
                 queryRef.addChildEventListener(new ChildEventListener() {
                     @Override
                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                         spinner.setVisibility(View.VISIBLE);
                         Produto produto = dataSnapshot.getValue(Produto.class);
-                        list.add(produto);
-                        adapter.notifyDataSetChanged();
-                        spinner.setVisibility(View.GONE);
 
-                        // Trata o TextView para mostrar a quantidade de produtos
-                        labelQuatidade = (TextView) findViewById(R.id.label_quantidade);
-                        String quantidade = list.size() + " produtos nesta categoria!";
-                        labelQuatidade.setText(quantidade);
+                        nomeProduto.setText(produto.getNome().toString());
+
+                        spinner.setVisibility(View.GONE);
                     }
 
                     @Override
@@ -131,5 +98,4 @@ public class DetalheCategoriaActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_market_car, menu);
         return true;
     }
-
 }
